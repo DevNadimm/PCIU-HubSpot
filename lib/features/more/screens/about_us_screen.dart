f010fec33bb8c9871724be69f7cb827fb2456b43
DevNadimm <nadimchowdhury87@gmail.com>
2024-10-26 20:54:31 +0600

    update

--- lib/features/more/screens/about_us_screen.dart
+++ lib/features/more/screens/about_us_screen.dart
@@ -1,14 +1,100 @@
 import 'package:flutter/material.dart';
+import 'package:pciu_hubspot/core/constants/colors.dart';
 
 class AboutUsScreen extends StatelessWidget {
   const AboutUsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      body: Center(
-        child: Text('About Us'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'About Us',
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
+        centerTitle: true,
       ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _buildSectionHeader(context, 'About PCIU HubSpot'),
+            const SizedBox(height: 8),
+            Text(
+              'The project aims to provide resources and information to students of PCIU, including a student name search, student CGPA, CR info, and officers info.',
+              style: Theme.of(context)
+                  .textTheme
+                  .bodyMedium!
+                  .copyWith(color: Colors.black87),
+              textAlign: TextAlign.justify,
+            ),
+            const SizedBox(height: 24),
+            _buildSectionHeader(context, 'Our Mission'),
+            const SizedBox(height: 8),
+            Text(
+              'Our mission is to centralize resources and make it easier for students to access important information, connect with their peers, and stay informed about academic progress.',
+              style: Theme.of(context)
+                  .textTheme
+                  .bodyMedium!
+                  .copyWith(color: Colors.black87),
+              textAlign: TextAlign.justify,
+            ),
+            const SizedBox(height: 24),
+            _buildSectionHeader(context, 'Meet Our Team'),
+            const SizedBox(height: 16),
+            _buildTeamMember(
+              imgPath: 'assets/images/member1.png',
+              name: 'Younus Foysal',
+              role: 'Project Leader',
+            ),
+            const SizedBox(height: 16),
+            _buildTeamMember(
+              imgPath: 'assets/images/member2.jpg',
+              name: 'Nadim Chowdhury',
+              role: 'Flutter Developer',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSectionHeader(BuildContext context, String title) {
+    return Text(title, style: Theme.of(context).textTheme.headlineLarge);
+  }
+
+  Widget _buildTeamMember(
+      {required String imgPath, required String name, required String role}) {
+    return Row(
+      children: [
+        CircleAvatar(
+          radius: 28,
+          backgroundImage: AssetImage(imgPath),
+          backgroundColor: primaryColor,
+        ),
+        const SizedBox(width: 16),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              name,
+              style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                color: Colors.black87,
+              ),
+            ),
+            Text(
+              role,
+              style: const TextStyle(
+                fontSize: 14,
+                color: Colors.black54,
+              ),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
