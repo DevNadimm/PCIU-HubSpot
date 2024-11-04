37c6c60760bdb0f9a6a5b0c97cf91a94590e667b
DevNadimm <nadimchowdhury87@gmail.com>
2024-11-05 00:01:26 +0600

    update

--- lib/features/home/screens/faculty_members_screen.dart
+++ lib/features/home/screens/faculty_members_screen.dart
@@ -2,11 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:pciu_hubspot/core/constants/faculty_members_data.dart';
 import 'package:pciu_hubspot/features/home/widgets/faculty_info_card.dart';
 
-class FacultyMembersScreen extends StatelessWidget {
+class FacultyMembersScreen extends StatefulWidget {
   const FacultyMembersScreen({super.key});
 
   @override
+  State<FacultyMembersScreen> createState() => _FacultyMembersScreenState();
+}
+
+class _FacultyMembersScreenState extends State<FacultyMembersScreen> {
+  final List<String> _departments = ['CSE', 'EEE', 'ENG'];
+  String? _selectedDepartment;
+  String _searchQuery = '';
+
+  @override
   Widget build(BuildContext context) {
+    final filteredFacultyList = facultyList.where((faculty) {
+      final matchesDepartment = _selectedDepartment == null ||
+          faculty.department == _selectedDepartment;
+      final matchesSearch =
+          faculty.name.toLowerCase().contains(_searchQuery.toLowerCase());
+      return matchesDepartment && matchesSearch;
+    }).toList();
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -22,12 +39,14 @@ class FacultyMembersScreen extends StatelessWidget {
           child: Column(
             children: [
               const SizedBox(height: 16),
+              _buildTopSection(context),
+              const SizedBox(height: 16),
               ListView.separated(
                 shrinkWrap: true,
                 physics: const NeverScrollableScrollPhysics(),
-                itemCount: facultyList.length,
+                itemCount: filteredFacultyList.length,
                 itemBuilder: (context, index) {
-                  final faculty = facultyList[index];
+                  final faculty = filteredFacultyList[index];
                   return FacultyInfoCard(
                     name: faculty.name,
                     shortName: faculty.shortForm,
@@ -38,9 +57,7 @@ class FacultyMembersScreen extends StatelessWidget {
                   );
                 },
                 separatorBuilder: (BuildContext context, int index) {
-                  return const SizedBox(
-                    height: 16,
-                  );
+                  return const SizedBox(height: 16);
                 },
               ),
               const SizedBox(height: 16),
@@ -48,6 +65,46 @@ class FacultyMembersScreen extends StatelessWidget {
           ),
         ),
       ),
+    );
+  }
+
+  Widget _buildTopSection(BuildContext context) {
+    return Row(
+      children: [
+        Flexible(
+          child: TextFormField(
+            style: Theme.of(context).textTheme.titleLarge,
+            decoration: const InputDecoration(hintText: 'Search by Name'),
+            onChanged: (value) {
+              setState(() {
+                _searchQuery = value;
+              });
+            },
+          ),
+        ),
+        const SizedBox(width: 8),
+        Flexible(
+          child: DropdownButtonFormField<String>(
+            isDense: true,
+            decoration: const InputDecoration(
+              contentPadding: EdgeInsets.all(14),
+              hintText: 'Department',
+            ),
+            value: _selectedDepartment,
+            onChanged: (newValue) {
+              setState(() {
+                _selectedDepartment = newValue;
+              });
+            },
+            items: _departments.map((department) {
+              return DropdownMenuItem<String>(
+                value: department,
+                child: Text(department),
+              );
+            }).toList(),
+          ),
+        ),
+      ],
     );
   }
 }
