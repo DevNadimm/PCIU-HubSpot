b58fba12913a09e50caa60cdcb7035609d4b8712
DevNadimm <nadimchowdhury87@gmail.com>
2024-11-05 10:23:37 +0600

    update

---
+++ lib/core/models/cr_model.dart
@@ -0,0 +1,21 @@
+class CR {
+  final String name;
+  final String? batch;
+  final String? department;
+  final String? shift;
+  final String? section;
+  final String? contact;
+  final String? email;
+  final String? facebookId;
+
+  CR({
+    required this.name,
+    this.batch,
+    this.shift,
+    this.section,
+    this.department,
+    this.contact,
+    this.email,
+    this.facebookId,
+  });
+}
