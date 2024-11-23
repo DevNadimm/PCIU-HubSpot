ab47dc4374332e84617e696b86b88219f523b857
DevNadimm <nadimchowdhury87@gmail.com>
2024-11-23 10:18:18 +0600

    update

---
+++ lib/shared/widgets/drop_down_munu_widget.dart
@@ -0,0 +1,51 @@
+import 'package:dropdown_button2/dropdown_button2.dart';
+import 'package:flutter/material.dart';
+
+class DropDownWidget<T> extends StatelessWidget {
+  final String? hintText;
+  final T? value;
+  final List<T> items;
+  final ValueChanged<T?> onChanged;
+
+  const DropDownWidget({
+    super.key,
+    required this.items,
+    required this.onChanged,
+    this.hintText,
+    this.value,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButtonFormField2<T>(
+      isDense: true,
+      value: value,
+      decoration: InputDecoration(
+        contentPadding: const EdgeInsets.only(top: 14, bottom: 14, right: 7),
+        hintText: hintText,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+        ),
+      ),
+      hint: Text(
+        hintText ?? '',
+        style: Theme.of(context)
+            .textTheme
+            .titleLarge!
+            .copyWith(color: Colors.black54),
+      ),
+      onChanged: onChanged,
+      items: items
+          .map(
+            (item) => DropdownMenuItem<T>(
+              value: item,
+              child: Text(
+                item.toString(),
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+            ),
+          )
+          .toList(),
+    );
+  }
+}
