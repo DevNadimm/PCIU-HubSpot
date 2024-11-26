09ce8aa528d965c230dbe6356b2575cf950bd9c1
DevNadimm <nadimchowdhury87@gmail.com>
2024-11-26 11:01:44 +0600

    update

--- lib/core/urls.dart
+++ lib/core/urls.dart
@@ -2,4 +2,8 @@ class Urls {
   static const String baseUrl = 'https://pciu-hubspot-server.vercel.app';
   static const String signUp = '$baseUrl/user';
   static const String signIn = '$baseUrl/jwt';
+
+  static String userDetails(String email) {
+    return '$baseUrl/user/$email';
+  }
 }
