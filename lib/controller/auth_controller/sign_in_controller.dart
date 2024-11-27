2d7cc21a6eba35570f8df64a54b013df623127b3
DevNadimm <nadimchowdhury87@gmail.com>
2024-11-27 20:31:44 +0600

    update

--- lib/controller/auth_controller/sign_in_controller.dart
+++ lib/controller/auth_controller/sign_in_controller.dart
@@ -7,8 +7,8 @@ import 'package:pciu_hubspot/core/urls.dart';
 class SignInController extends GetxController {
   static final instance = Get.find<SignInController>();
 
-  bool _inProgress = false;
-  bool get inProgress => _inProgress;
+  bool _signInInProgress = false;
+  bool get signInInProgress => _signInInProgress;
 
   String? _errorMessage;
   String? get errorMessage => _errorMessage;
@@ -16,7 +16,7 @@ class SignInController extends GetxController {
   bool isSuccess = false;
 
   Future<bool> signIn({required String email, required String password}) async {
-    _inProgress = true;
+    _signInInProgress = true;
     update();
 
     Map<String, dynamic> requestBody = {
@@ -31,25 +31,23 @@ class SignInController extends GetxController {
       );
 
       if (response.isSuccess) {
-        isSuccess = true;
-
         final data = response.responseData;
         if (data.containsKey('token')) {
           final token = data['token'];
           await AuthController.saveAccessToken(token);
+          isSuccess = true;
         } else {
           debugPrint('Token not found in response');
+          _errorMessage = 'We couldn’t log you in. Please try again later.';
         }
       } else {
-        isSuccess = false;
-        _errorMessage = 'Sign-in failed. Please check your credentials and try again.';
+        _errorMessage = 'Sign-in failed. Please check your email and password and try again.';
       }
     } catch (e) {
-      isSuccess = false;
-      _errorMessage = 'An error occurred: $e. Please try again later.';
+      _errorMessage = 'Something went wrong. Please check your internet connection and try again later.';
     }
 
-    _inProgress = false;
+    _signInInProgress = false;
     update();
 
     return isSuccess;
