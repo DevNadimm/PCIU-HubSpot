07b592178585ea9ec79fd72de021cad84c9a8778
DevNadimm <nadimchowdhury87@gmail.com>
2024-11-29 20:51:06 +0600

    update

--- lib/generate_cover_page/screens/pdf_preview_page.dart
+++ lib/generate_cover_page/screens/pdf_preview_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_pdfview/flutter_pdfview.dart';
 import 'package:open_file/open_file.dart';
-import 'dart:io';
 import 'package:pciu_hubspot/core/utils/snackbar_message.dart';
+import 'dart:io';
 
 class PdfPreviewPage extends StatelessWidget {
   final String pdfPath;
@@ -25,48 +25,64 @@ class PdfPreviewPage extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            Expanded(
-              child: File(pdfPath).existsSync()
-                  ? Center(
-                      child: Container(
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          border: Border.all(
-                            color: Colors.grey.shade400,
-                            width: 2.0,
-                          ),
-                          borderRadius: BorderRadius.circular(8.0),
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: AspectRatio(
-                            aspectRatio: 1 / 1.414,
-                            child: PDFView(
-                              filePath: pdfPath,
-                            ),
-                          ),
-                        ),
-                      ),
-                    )
-                  : const Center(
-                      child: Text("Error: PDF file not found"),
-                    ),
-            ),
-            const SizedBox(height: 20),
-            SizedBox(
-              width: double.infinity,
-              height: 50,
-              child: ElevatedButton(
-                onPressed: () {
-                  _openPdf(pdfPath, context);
-                },
-                child: const Text('Open PDF'),
+        child: File(pdfPath).existsSync() ? _viewPdf(context) : _errorMsg(context),
+      ),
+    );
+  }
+
+  Widget _viewPdf(BuildContext context) {
+    return Column(
+      children: [
+        Expanded(
+          child: Center(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(
+                  color: Colors.grey.shade400,
+                  width: 2.0,
+                ),
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: AspectRatio(
+                  aspectRatio: 1 / 1.414,
+                  child: PDFView(
+                    filePath: pdfPath,
+                  ),
+                ),
               ),
             ),
-          ],
+          ),
         ),
+        const SizedBox(height: 16),
+        SizedBox(
+          width: double.infinity,
+          height: 50,
+          child: ElevatedButton(
+            onPressed: () {
+              _openPdf(pdfPath, context);
+            },
+            child: const Text('Open PDF'),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _errorMsg(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset("assets/images/empty_data.png", scale: 10),
+          const SizedBox(height: 10),
+          Text(
+            "PDF file not found!",
+            style: Theme.of(context).textTheme.titleMedium,
+          ),
+        ],
       ),
     );
   }
