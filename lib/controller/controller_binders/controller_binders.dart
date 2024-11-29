4f1d91498e502de6c301151654e48bec90f4c5fa
DevNadimm <nadimchowdhury87@gmail.com>
2024-11-29 10:11:40 +0600

    update

--- lib/controller/controller_binders/controller_binders.dart
+++ lib/controller/controller_binders/controller_binders.dart
@@ -1,6 +1,7 @@
 import 'package:get/get.dart';
 import 'package:pciu_hubspot/controller/auth_controller/google_sign_in_controller.dart';
 import 'package:pciu_hubspot/controller/auth_controller/sign_in_controller.dart';
+import 'package:pciu_hubspot/controller/home_controller/home_controller.dart';
 import 'package:pciu_hubspot/controller/network_controller/network_controller.dart';
 import '../auth_controller/sign_up_controller.dart';
 
@@ -11,5 +12,6 @@ class ControllerBinders extends Bindings{
     Get.put(SignInController());
     Get.put(NetworkController());
     Get.put(GoogleSignInController());
+    Get.put(HomeController());
   }
 }
