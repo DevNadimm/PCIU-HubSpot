fbcc5e02c82f21f07ce49c39aa05078abc3d97ac
DevNadimm <nadimchowdhury87@gmail.com>
2024-11-30 22:36:49 +0600

    update

--- lib/controller/auth_controller/sign_in_controller.dart
+++ lib/controller/auth_controller/sign_in_controller.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:pciu_hubspot/controller/home_controller/student_details_controller.dart';
 import 'package:pciu_hubspot/controller/shared_preferences_controller/auth_controller_prefs.dart';
 import 'package:pciu_hubspot/core/network/network_caller.dart';
 import 'package:pciu_hubspot/core/urls.dart';
@@ -19,12 +20,41 @@ class SignInController extends GetxController {
     _signInInProgress = true;
     update();
 
-    Map<String, dynamic> requestBody = {
-      "identifier": email,
-      "password": password,
-    };
-
     try {
+      final response = await NetworkCaller.getRequest(url: Urls.userDetails(email));
+
+      if (response.isSuccess) {
+        final data = response.responseData;
+
+        if (data.containsKey('data') && data['data'] == 'UserNotFound') {
+          _handleUserNotFound(email);
+          return false;
+        } else {
+          final sid = data['sid'];
+          final userEmail = data['email'];
+          final name = data['name'];
+
+          return await _signInUser(email: userEmail, name: name, sid: sid, password: password);
+        }
+      } else {
+        return _handleSignInError('We encountered an issue while fetching your details. Please try again.');
+      }
+    } catch (e) {
+      return _handleSignInError('An error occurred during the sign-in process. Please try again.');
+    } finally {
+      _signInInProgress = false;
+      update();
+    }
+  }
+
+  Future<bool> _signInUser({required String email, required String name, required String password, required String sid}) async {
+    try {
+      Map<String, dynamic> requestBody = {
+        "identifier": email,
+        "password": password
+      };
+      debugPrint('Signing in with: EMAIL: $email\n, NAME: $name\n, SID: $sid\n');
+
       final response = await NetworkCaller.postRequest(
         url: Urls.signIn,
         body: requestBody,
@@ -35,21 +65,38 @@ class SignInController extends GetxController {
         if (data.containsKey('token')) {
           final token = data['token'];
           await AuthController.saveAccessToken(token);
+
+          final studentDetailsController = StudentDetailsController.instance;
+          await studentDetailsController.getStudentDetails(
+            email: email,
+            nameOnEmail: name,
+            sId: sid,
+          );
+
           isSuccess = true;
         } else {
           debugPrint('Token not found in response');
           _errorMessage = 'We couldn’t log you in. Please try again later.';
+          isSuccess = false;
         }
       } else {
         _errorMessage = 'Sign-in failed. Please check your email and password and try again.';
+        isSuccess = false;
       }
     } catch (e) {
       _errorMessage = 'Something went wrong. Please check your internet connection and try again later.';
+      isSuccess = false;
     }
-
-    _signInInProgress = false;
-    update();
-
     return isSuccess;
+  }
+
+  void _handleUserNotFound(String email) {
+    isSuccess = false;
+    _errorMessage = 'User not found for email $email.';
+  }
+
+  bool _handleSignInError(String error) {
+    _errorMessage = error;
+    return false;
   }
 }
