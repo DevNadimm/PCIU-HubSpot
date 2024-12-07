fee82c890aadf8f8a3fcf609294b451315da9df8
DevNadimm <nadimchowdhury87@gmail.com>
2024-12-07 22:07:23 +0600

    update

--- lib/features/home/screens/faculty_members_screen.dart
+++ lib/features/home/screens/faculty_members_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:pciu_hubspot/core/constants/faculty_members_data.dart';
+import 'package:get/get.dart';
+import 'package:pciu_hubspot/controller/home_controller/faculty_controller.dart';
+import 'package:pciu_hubspot/core/models/faculty_model.dart';
+import 'package:pciu_hubspot/core/utils/progress_indicator.dart';
 import 'package:pciu_hubspot/features/home/widgets/faculty_info_card.dart';
 import 'package:pciu_hubspot/shared/widgets/dropdown_menu_widget.dart';
 import 'package:pciu_hubspot/shared/widgets/empty_list_widget.dart';
@@ -13,16 +16,29 @@ class FacultyMembersScreen extends StatefulWidget {
 
 class _FacultyMembersScreenState extends State<FacultyMembersScreen> {
   final List<String> _departments = ['CSE', 'EEE', 'ENG'];
+  FacultyController facultyController = FacultyController.instance;
+  List<Faculties> facultyList = [];
   String? _selectedDepartment;
   String _searchQuery = '';
 
   @override
+  void initState() {
+    fetchList();
+    super.initState();
+  }
+
+  void fetchList () async {
+    await facultyController.getFacultyMember();
+    facultyList = facultyController.facultyList!;
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final filteredFacultyList = facultyList.where((faculty) {
+    final filteredFacultyList = facultyList!.where((faculty) {
       final matchesDepartment = _selectedDepartment == null ||
           faculty.department == _selectedDepartment;
       final matchesSearch =
-          faculty.name.toLowerCase().contains(_searchQuery.toLowerCase());
+          faculty.name!.toLowerCase().contains(_searchQuery.toLowerCase());
       return matchesDepartment && matchesSearch;
     }).toList();
 
@@ -37,36 +53,44 @@ class _FacultyMembersScreenState extends State<FacultyMembersScreen> {
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16.0),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              const SizedBox(height: 16),
-              _buildTopSection(context),
-              const SizedBox(height: 16),
-              filteredFacultyList.isEmpty
-                  ? const EmptyListWidget()
-                  : ListView.separated(
-                      shrinkWrap: true,
-                      physics: const NeverScrollableScrollPhysics(),
-                      itemCount: filteredFacultyList.length,
-                      itemBuilder: (context, index) {
-                        final faculty = filteredFacultyList[index];
-                        return FacultyInfoCard(
-                          name: faculty.name,
-                          shortName: faculty.shortForm,
-                          designation: faculty.designation,
-                          room: faculty.room,
-                          department: faculty.department,
-                          contact: faculty.contact,
-                        );
-                      },
-                      separatorBuilder: (BuildContext context, int index) {
-                        return const SizedBox(height: 16);
-                      },
-                    ),
-              const SizedBox(height: 16),
-            ],
-          ),
+        child: GetBuilder<FacultyController>(
+          builder: (controller) {
+            return Visibility(
+              visible: !controller.inProgress,
+              replacement: const ProgressIndicatorWidget(),
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    const SizedBox(height: 16),
+                    _buildTopSection(context),
+                    const SizedBox(height: 16),
+                    filteredFacultyList.isEmpty
+                        ? const EmptyListWidget()
+                        : ListView.separated(
+                            shrinkWrap: true,
+                            physics: const NeverScrollableScrollPhysics(),
+                            itemCount: filteredFacultyList.length,
+                            itemBuilder: (context, index) {
+                              final faculty = filteredFacultyList[index];
+                              return FacultyInfoCard(
+                                name: faculty.name ?? 'N/A',
+                                shortName: faculty.sform ?? 'N/A',
+                                designation: faculty.designation ?? 'N/A',
+                                room: faculty.room ?? 'N/A',
+                                department: faculty.department ?? 'N/A',
+                                contact: faculty.contact ?? 'N/A',
+                              );
+                            },
+                            separatorBuilder: (BuildContext context, int index) {
+                              return const SizedBox(height: 16);
+                            },
+                          ),
+                    const SizedBox(height: 16),
+                  ],
+                ),
+              ),
+            );
+          }
         ),
       ),
     );
