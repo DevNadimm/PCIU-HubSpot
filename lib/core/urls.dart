2492f0ba1fb4fcc23a94a45fb4c7b728f5561149
DevNadimm <nadimchowdhury87@gmail.com>
2024-12-10 23:37:56 +0600

    update

--- lib/core/urls.dart
+++ lib/core/urls.dart
@@ -4,6 +4,7 @@ class Urls {
   static const String signIn = '$baseUrl/jwt';
   static const String emptyRoom = '$baseUrl/emptyroomAPP';
   static const String contactAdmin = '$baseUrl/msg';
+  static const String writeReview = '$baseUrl/reviews';
 
   static String userDetails(String email) {
     return '$baseUrl/user/$email';
