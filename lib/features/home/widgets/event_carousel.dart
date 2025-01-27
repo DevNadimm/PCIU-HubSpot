7d68a9a347a456f00407a6a635f28fb94adbe090
DevNadimm <nadimchowdhury87@gmail.com>
2025-01-27 21:02:24 +0600

    update

--- lib/features/home/widgets/event_carousel.dart
+++ lib/features/home/widgets/event_carousel.dart
@@ -1,10 +1,11 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:pciu_hubspot/core/constants/colors.dart';
 import 'package:pciu_hubspot/core/models/event_model.dart';
-import 'package:pciu_hubspot/features/home/widgets/upcoming_event_card.dart';
 
 class EventCarousel extends StatelessWidget {
   const EventCarousel({super.key, required this.eventList});
+
   final List<EventModel> eventList;
 
   @override
@@ -17,14 +18,101 @@ class EventCarousel extends StatelessWidget {
         ),
       );
     }
+
     return CarouselSlider(
       options: CarouselOptions(
+        height: 170,
         autoPlay: true,
         enlargeCenterPage: true,
         viewportFraction: 0.8,
       ),
       items: eventList.map((event) {
-        return UpcomingEventCard(event: event);
+        return Container(
+          decoration: BoxDecoration(
+            gradient: const LinearGradient(
+              colors: [primaryColor, secondaryColor],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+            borderRadius: BorderRadius.circular(12),
+            border: Border.all(width: 1, color: Colors.white),
+          ),
+          child: Stack(
+            children: [
+              Positioned(
+                right: MediaQuery.of(context).size.width * -0.2,
+                bottom: MediaQuery.of(context).size.height * -0.05,
+                child: Container(
+                  height: 200,
+                  width: 200,
+                  decoration: BoxDecoration(
+                    color: Colors.white.withOpacity(0.1),
+                    borderRadius: BorderRadius.circular(100),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      "Upcoming Event",
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.white.withOpacity(0.8),
+                      ),
+                    ),
+                    const SizedBox(height: 4),
+                    Text(
+                      event.name ?? "",
+                      style: const TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    Row(
+                      children: [
+                        const Icon(Icons.calendar_today_outlined,
+                            color: Colors.white, size: 18),
+                        const SizedBox(width: 8),
+                        Text(
+                          event.date ?? "",
+                          style: const TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 4),
+                    Row(
+                      children: [
+                        const Icon(Icons.access_time,
+                            color: Colors.white, size: 18),
+                        const SizedBox(width: 8),
+                        Text(
+                          event.time ?? "",
+                          style: const TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        );
       }).toList(),
     );
   }
